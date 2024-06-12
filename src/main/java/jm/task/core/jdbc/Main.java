dd381ea7586e522a06159383f5ae9e7b2e73d6e7
IsItSure <noobukgames@gmail.com>
2024-06-12 15:22:37 +0300

    Done

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,9 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
-        
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Alex", "Vanin", (byte) 24);
+        userService.saveUser("Ivan", "Mevan", (byte) 44);
+        userService.saveUser("Darya", "Avonavi", (byte) 19);
+
+        for (User user : userService.getAllUsers()) {
+            System.out.println(user);
+        }
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
